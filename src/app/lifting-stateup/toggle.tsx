85bcd4be40f9638cfcc97d22848e7d1332bedde3
정지현 <mujuk73@hotmail.com>
2025-01-23 10:09:24 +0900

    상태보다는 속성 활용

--- src/app/lifting-stateup/toggle.tsx
+++ src/app/lifting-stateup/toggle.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 type ToggleProps = {
   isOn: boolean;
   onClick: () => void;
@@ -15,3 +17,25 @@ const Toggle = ({ isOn, onClick }: ToggleProps) => {
     </button>
   );
 };
+
+const StatusDisplay = ({ isOn }: { isOn: boolean }) => {
+  return (
+    <div className={`text-lg ${isOn ? "text-blue-500" : "text-red-100"}`}>
+      {isOn ? "상태 : 켜짐" : "상태 : 꺼짐"}
+    </div>
+  );
+};
+
+const ToggleParentComponent = () => {
+  const [isToggled, setIsToggled] = useState(false);
+  return (
+    <div
+      className={`flex flex-col items-center justify-center space-y-4 p-4 ${isToggled ? "bg-blue-100" : "bg-red-600"}`}
+    >
+      <Toggle isOn={isToggled} onClick={() => setIsToggled(!isToggled)} />
+      <StatusDisplay isOn={isToggled} />
+    </div>
+  );
+};
+
+export default ToggleParentComponent;
